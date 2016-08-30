f180df89372c7880a0b0f66cc4f6386d8c956272
Marco van 't Wout <mr@vtwout.net>
2016-08-30 09:41:33 +0200

    Check if vendor path exists before requiring, to prevent obscure error message.

--- XPHPExcel.php
+++ XPHPExcel.php
@@ -14,8 +14,13 @@ class XPHPExcel extends CComponent
 	public static function init()
 	{
 		if (!self::$_isInitialized) {
+			$baseFile = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'PHPExcel.php';
+			if (!file_exists($baseFile)) {
+				throw new CException('Cannot find PHPExcel.php in expected path.');
+			}
+			
 			spl_autoload_unregister(array('YiiBase', 'autoload'));
-			require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'PHPExcel.php');
+			require($baseFile);
 			spl_autoload_register(array('YiiBase', 'autoload'));
 			
 			self::$_isInitialized = true;
